4bed8c7cebda3351eb76dad94b60dee9439e3847
Han-Bin Song <thdgksq@naver.com>
2025-02-25 13:41:10 +0900

    순서를 추가하여 'O' 표시하기

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,11 +4,14 @@ import Square from './Square'
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+  const status = `Next player: ${xIsNext ? 'X' : 'O'}`
 
   const handleClick = (i) => {
     const newSquares = squares.slice()
-    newSquares[i] = 'X'
+    newSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(newSquares)
+    setXIsNext(prev => !prev)
   }
 
   const renderSquare = (i) => {
@@ -17,7 +20,7 @@ const Board = () => {
 
   return( 
       <div>
-        <div className='status'>Next player: X, O</div>
+        <div className='status'>{status}</div>
         <div className='board-row'>
           {renderSquare(0)}
           {renderSquare(1)}
