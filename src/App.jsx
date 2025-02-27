cff5a980b786d01f81ff85a89cadba7810be84f9
Han-Bin Song <thdgksq@naver.com>
2025-02-27 12:27:43 +0900

    스타일링 변경

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,9 @@ function App() {
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
       </li>
     );
   });
@@ -70,7 +72,7 @@ function App() {
       </div>
       <div className="game-info">
         <div className="status">{status}</div>
-        <ol>{moves}</ol>
+        <ol style={{ listStyle: 'none' }}>{moves}</ol>
       </div>
     </div>
   );
